b7feb6b747eee8598d750dfa7ccb6b74db4490af
DartPower <dartpower@mail.ru>
2019-03-19 09:26:28 +0300

    Beutify PowerShell Script

--- Setup.ps1
+++ Setup.ps1
@@ -1,73 +1,73 @@
 ﻿function Main() {
-    Write-Host "==== ts4multiplayer ===="
+	Write-Host "==== ts4multiplayer ===="
 
-    $config = @{
-        mode = GetModeString (PromptUntilValid 'Server (s) or Client (c)' '[sc]')
-        host = Read-Host -Prompt 'Host'
-        port = Read-Host -Prompt 'Port'
-    }
+	$config = @{
+		mode = GetModeString (PromptUntilValid 'Server (s) or Client (c)' '[sc]')
+		host = Read-Host -Prompt 'Host'
+		port = Read-Host -Prompt 'Port'
+	}
 
-    SetupMod (New-Object -TypeName psobject -Prop $config)
+	SetupMod (New-Object -TypeName psobject -Prop $config)
 
-    return
+	return
 }
 
 function SetupMod($config) {
-    CreateMarkerFile $config
+	CreateMarkerFile $config
 
-    UpdateConfig $config
+	UpdateConfig $config
 }
 
 function GetModeString ($mode) {
-    If ($mode -match 's') {
-        return 'server'
-    }
-    Else {
-        return 'client'
-    }
+	If ($mode -match 's') {
+		return 'server'
+	}
+	Else {
+		return 'client'
+	}
 }
 
-$coreDirectory = 'Scripts\ts4mp\core'
+$coreDirectory = 'srcScripts\ts4mp\core'
 
 function CreateMarkerFile($config) {
-    ClearExistingSetup
+	ClearExistingSetup
 
-    $mode = $config.mode
+	$mode = $config.mode
 
-    New-Item "Scripts\ts4mp\core\$mode.txt"
+	New-Item "srcScripts\ts4mp\core\$mode.txt"
 }
 
 function ClearExistingSetup() {
-    $paths = ".\$coreDirectory\client.txt", ".\$coreDirectory\server.txt"
+	$paths = ".\$coreDirectory\client.txt", ".\$coreDirectory\server.txt"
 
-    Write-Host "Checking for $paths"
+	Write-Host "Checking for $paths"
 
-    ForEach ($path in $paths) {
-        If (Test-Path $path) {
-            Write-Host "Removing $path"
-            Remove-Item $path | Out-Null
-        }
-    }
+	ForEach ($path in $paths) {
+		If (Test-Path $path) {
+			Write-Host "Removing $path"
+			Remove-Item $path | Out-Null
+		}
+	}
 }
 
 function UpdateConfig($config) {
-    $mode = $config.mode
-    $sourceFilePath = Resolve-Path "$coreDirectory\multiplayer_$mode.py"
+	$mode = $config.mode
+	$sourceFilePath = Resolve-Path "$coreDirectory\multiplayer_$mode.py"
 
-    Write-Host "Updating $sourceFilePath $config"
+	Write-Host "Updating $sourceFilePath $config"
 
-    (Get-Content -Path "$sourceFilePath.tmp") | ForEach-Object {$_ -Replace '\$\{serverHost\}', $config.host -Replace '\$\{serverPort\}', $config.port} | Set-Content -Path $sourceFilePath
+	(Get-Content -Path "$sourceFilePath.tmp") | ForEach-Object {$_ -Replace '\$\{serverHost\}', $config.host -Replace '\$\{serverPort\}', $config.port} | Set-Content -Path $sourceFilePath
 }
 
 function PromptUntilValid($prompt, $pattern) {
-    $value = $null
-    While (($value = Prompt $prompt) -notmatch $pattern) {}
+	$value = $null
+	While (($value = Prompt $prompt) -notmatch $pattern) {}
 
-    return $value
+	return $value
 }
 
 function Prompt($prompt) {
-    return (Read-Host -Prompt $prompt)
+	return (Read-Host -Prompt $prompt)
 }
 
 Main
